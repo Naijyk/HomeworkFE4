9c90fa0a1529454f834fa545433f2ad54ca6b1fc
Naijyk <133905330+Naijyk@users.noreply.github.com>
2023-08-17 10:25:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// Задание 1
+
+// let d = 7;
+
+// console.log(d);
+// console.log(d ** 2);
+// console.log(d ** 3);
+
+// Задание 2
+
+// let q = Number(prompt('Введите целое число'));
+
+// if(q > 0){
+//     console.log(q * 2);
+// }
+// else{
+//     alert('Error');
+// }
+
+// Задание 3
+
+// let w = Number(prompt('Введите первое целое число'));
+// let e = Number(prompt('Введите второе целое число'));
+
+// if(w > e){
+//     console.log(w + e);
+// }
+// else if(w < e){
+//     console.log(w * e);
+// }
+// else{
+//     alert('Числа одинаковые');
+// }
+
+// Задание 4
+
+// let r = Number(prompt('Введите целое число'));
+
+// if(r < 0){
+//     console.log(r ** 2);
+// }
+// else{
+//     alert('Error');
+// }
+
+// Задание 5
+
+// let t = Number(prompt('Введите количество учеников'));
+// let y = Number(prompt('Введите количество стульев'));
+
+// if(t <= y){
+//     alert('Стульев хватает');
+// }
+// else if(t > y){
+//     alert('Стульев не хватает');
+// }
